67ee8e3f8de8120ed6b7a97c53295d153999ec6f
bkgoh27 <bengkoon.goh27@gmail.com>
2022-03-29 16:43:42 +0800

    go will double the capacity for you

--- slices.go
+++ slices.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
-	//Insert Code here
+	spending := []float64{9.5, 8.0, 10.2, 7.4}
+	fmt.Println(spending)
+	fmt.Println(len(spending))
+	fmt.Println(cap(spending))
+
+	spending = append(spending, 7.0)
+	fmt.Println(len(spending))
+	fmt.Println(cap(spending))
 }
